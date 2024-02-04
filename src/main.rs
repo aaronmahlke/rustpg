a2225c189286a6fff61f0003c9019190266401fa
Aaron Mahlke <aaron.mahlke@gmail.com>
2024-02-04 14:53:25 +0100

    Add minimal enemies

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::FrameTimeDiagnosticsPlugin,
+    prelude::*,
+    window::{PresentMode, WindowTheme},
+};
+
+mod fps;
+use crate::fps::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "bevyaac".into(),
+                        present_mode: PresentMode::Immediate,
+                        prevent_default_event_handling: false,
+                        window_theme: Some(WindowTheme::Dark),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_plugins((FrameTimeDiagnosticsPlugin, FPSPlugin))
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-        .add_systems(Startup, (setup_system, spawn_player))
+        .add_systems(Startup, (setup_system, spawn_player, spawn_enemies))
         .add_systems(
             Update,
             (
@@ -13,19 +34,16 @@ fn main() {
                 flip_player,
                 player_shoot,
                 update_bullets,
+                move_enemy,
             ),
         )
         .run();
 }
 
-pub const MOVE_SPEED: f32 = 500.0;
-pub const PLAYER_SIZE: f32 = 5.0;
-
 #[derive(Component)]
 struct AnimationIndices {
     first: usize,
     last: usize,
-    time: f32,
 }
 
 #[derive(Component, Deref, DerefMut)]
@@ -38,6 +56,7 @@ struct ShootTimer(Timer);
 struct Bullet {
     direction: Vec3,
     speed: f32,
+    size: f32,
 }
 
 #[derive(Component)]
@@ -45,6 +64,15 @@ struct Player {
     idle: AnimationIndices,
     walk: AnimationIndices,
     state: PlayerState,
+    stats: PlayerStats,
+}
+
+struct PlayerStats {
+    size: f32,
+    shot_speed: f32,
+    shot_damage: f32,
+    move_speed: f32,
+    health: i8,
 }
 
 struct PlayerState {
@@ -58,16 +86,64 @@ impl Default for Player {
             idle: AnimationIndices {
                 first: 241,
                 last: 241,
-                time: 0.1,
             },
             walk: AnimationIndices {
                 first: 242,
                 last: 243,
-                time: 0.1,
             },
             state: PlayerState {
                 moving: false,
                 facing: Vec3::new(1.0, 0.0, 0.0),
+            },
+            stats: PlayerStats {
+                size: 5.0,
+                shot_speed: 0.4,
+                shot_damage: 1.0,
+                move_speed: 300.0,
+                health: 10,
+            },
+        }
+    }
+}
+
+#[derive(Component)]
+struct Enemy {
+    idle: AnimationIndices,
+    walk: AnimationIndices,
+    state: EnemyState,
+    stats: EnemyStats,
+}
+
+struct EnemyState {
+    moving: bool,
+    facing: Vec3,
+}
+
+struct EnemyStats {
+    size: f32,
+    move_speed: f32,
+    health: i8,
+}
+
+impl Default for Enemy {
+    fn default() -> Self {
+        Self {
+            idle: AnimationIndices {
+                first: 321,
+                last: 321,
+            },
+            walk: AnimationIndices {
+                first: 322,
+                last: 324,
+            },
+            state: EnemyState {
+                moving: false,
+                facing: Vec3::new(1.0, 0.0, 0.0),
+            },
+            stats: EnemyStats {
+                size: 5.0,
+                move_speed: 100.0,
+                health: 3,
             },
         }
     }
@@ -95,12 +171,49 @@ fn spawn_player(
         SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
             sprite: TextureAtlasSprite::new(player.idle.first),
-            transform: Transform::from_scale(Vec3::splat(PLAYER_SIZE)),
+            transform: Transform::from_scale(Vec3::splat(player.stats.size)),
             ..default()
         },
-        AnimationTimer(Timer::from_seconds(player.idle.time, TimerMode::Repeating)),
-        ShootTimer(Timer::from_seconds(0.5, TimerMode::Once)),
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        ShootTimer(Timer::from_seconds(
+            player.stats.shot_speed,
+            TimerMode::Once,
+        )),
         player,
+    ));
+}
+
+fn spawn_enemies(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("monochrome_tilemap_transparent.png");
+    let texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(16.0, 16.0),
+        20,
+        20,
+        Some(Vec2::new(1.0, 1.0)),
+        None,
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    let enemy = Enemy::default();
+
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(enemy.idle.first),
+            transform: Transform {
+                translation: Vec3::new(100.0, 100.0, 0.0),
+                scale: Vec3::splat(enemy.stats.size),
+                ..default()
+            },
+            ..default()
+        },
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        enemy,
     ));
 }
 
@@ -136,9 +249,9 @@ fn animate_player(
 fn flip_player(mut query: Query<(&Player, &mut Transform, &mut TextureAtlasSprite)>) {
     for (player, mut transform, _sprite) in &mut query {
         if player.state.facing.x < 0.0 {
-            transform.scale.x = -PLAYER_SIZE;
+            transform.scale.x = -player.stats.size;
         } else {
-            transform.scale.x = PLAYER_SIZE;
+            transform.scale.x = player.stats.size;
         }
     }
 }
@@ -167,10 +280,23 @@ fn move_player(
         if movement.length() > 0.0 {
             player.state.moving = true;
             player.state.facing = movement.normalize();
-            transform.translation += movement * MOVE_SPEED * time.delta_seconds();
+            transform.translation += movement * player.stats.move_speed * time.delta_seconds();
         } else {
             player.state.moving = false;
         }
+    }
+}
+
+fn move_enemy(
+    time: Res<Time>,
+    mut query: Query<(&mut Enemy, &mut Transform)>,
+    player_query: Query<&Transform, (With<Player>, Without<Enemy>)>,
+) {
+    let player_transform = player_query.single();
+    for (enemy, mut transform) in &mut query {
+        let direction = player_transform.translation - transform.translation;
+        let movement = direction.normalize();
+        transform.translation += movement * enemy.stats.move_speed * time.delta_seconds();
     }
 }
 
@@ -184,7 +310,7 @@ fn player_shoot(
 ) {
     let mut shooting: bool = false;
     let mut direction: Vec3 = Vec3::ZERO;
-    for (_player, transform, mut shoot_timer) in &mut query {
+    for (player, transform, mut shoot_timer) in &mut query {
         if keyboard_input.pressed(KeyCode::Left) {
             shooting = true;
             direction = Vec3::new(-1.0, 0.0, 0.0);
@@ -204,12 +330,13 @@ fn player_shoot(
         if shooting {
             let duration: f32 = shoot_timer.elapsed_secs();
 
-            if duration >= 0.5 {
+            if duration >= player.stats.shot_speed {
                 shoot_timer.reset();
                 // spawn bullet
                 let bullet = Bullet {
                     direction,
                     speed: 1000.0,
+                    size: 10.0,
                 };
 
                 // Rectangle
@@ -217,7 +344,7 @@ fn player_shoot(
                     SpriteBundle {
                         sprite: Sprite {
                             color: Color::rgb(255., 255., 255.),
-                            custom_size: Some(Vec2::new(5.0, 5.0)),
+                            custom_size: Some(Vec2::new(bullet.size, bullet.size)),
                             ..default()
                         },
                         transform: Transform::from_translation(Vec3::new(
@@ -240,4 +367,12 @@ fn update_bullets(time: Res<Time>, mut query: Query<(&Bullet, &mut Transform)>) 
     for (bullet, mut transform) in &mut query {
         transform.translation += bullet.direction * bullet.speed * time.delta_seconds();
     }
+}
+
+fn enemy_chase_player(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut eneym_query: Query<(&Transform, &mut Enemy)>,
+    player_query: Query<&Transform, With<Player>>,
+) {
 }
