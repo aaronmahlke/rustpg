ad1f14840deb172194fc604e6b4093f33b5993dd
Aaron Mahlke <aaron.mahlke@gmail.com>
2024-02-07 12:09:15 +0100

    Add collision with rapier

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use bevy::{
     window::{PresentMode, WindowTheme},
 };
 
+use bevy_rapier2d::prelude::*;
+
 mod fps;
 use crate::fps::*;
 
@@ -23,6 +25,8 @@ fn main() {
                 })
                 .set(ImagePlugin::default_nearest()),
         )
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins((FrameTimeDiagnosticsPlugin, FPSPlugin))
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_systems(Startup, (setup_system, spawn_player, spawn_enemies))
@@ -35,6 +39,7 @@ fn main() {
                 player_shoot,
                 update_bullets,
                 move_enemy,
+                hurt_enemy,
             ),
         )
         .run();
@@ -51,6 +56,9 @@ struct AnimationTimer(Timer);
 
 #[derive(Component, Deref, DerefMut)]
 struct ShootTimer(Timer);
+
+#[derive(Component, Deref, DerefMut)]
+struct BulletDespawnTimer(Timer);
 
 #[derive(Component)]
 struct Bullet {
@@ -143,7 +151,7 @@ impl Default for Enemy {
             stats: EnemyStats {
                 size: 5.0,
                 move_speed: 100.0,
-                health: 3,
+                health: 20,
             },
         }
     }
@@ -171,7 +179,11 @@ fn spawn_player(
         SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
             sprite: TextureAtlasSprite::new(player.idle.first),
-            transform: Transform::from_scale(Vec3::splat(player.stats.size)),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::splat(player.stats.size),
+                ..Default::default()
+            },
             ..default()
         },
         AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
@@ -179,7 +191,12 @@ fn spawn_player(
             player.stats.shot_speed,
             TimerMode::Once,
         )),
-        player,
+        RigidBody::Dynamic,
+        Velocity::zero(),
+        Collider::ball(player.stats.size),
+        Player::default(),
+        ActiveEvents::COLLISION_EVENTS,
+        LockedAxes::ROTATION_LOCKED,
     ));
 }
 
@@ -201,20 +218,28 @@ fn spawn_enemies(
 
     let enemy = Enemy::default();
 
-    commands.spawn((
-        SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            sprite: TextureAtlasSprite::new(enemy.idle.first),
-            transform: Transform {
-                translation: Vec3::new(100.0, 100.0, 0.0),
-                scale: Vec3::splat(enemy.stats.size),
+    commands
+        .spawn((
+            SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                sprite: TextureAtlasSprite::new(enemy.idle.first),
+                transform: Transform {
+                    translation: Vec3::new(100.0, 100.0, 0.0),
+                    scale: Vec3::splat(enemy.stats.size),
+                    ..default()
+                },
                 ..default()
             },
-            ..default()
-        },
-        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-        enemy,
-    ));
+            AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+            LockedAxes::ROTATION_LOCKED,
+            ActiveEvents::COLLISION_EVENTS,
+            enemy,
+        ))
+        .insert((
+            Collider::ball(Enemy::default().stats.size),
+            RigidBody::Dynamic,
+            Velocity::zero(),
+        ));
 }
 
 fn setup_system(mut commands: Commands) {
@@ -257,46 +282,80 @@ fn flip_player(mut query: Query<(&Player, &mut Transform, &mut TextureAtlasSprit
 }
 
 fn move_player(
-    time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Player, &mut Transform)>,
+    mut query: Query<(&mut Player, &mut Velocity)>,
 ) {
-    for (mut player, mut transform) in &mut query {
-        let mut movement = Vec3::ZERO;
-        if keyboard_input.pressed(KeyCode::W) {
-            movement.y += 1.0;
+    for (mut player, mut vel) in &mut query {
+        let up = keyboard_input.any_pressed([KeyCode::W]);
+        let down = keyboard_input.any_pressed([KeyCode::S]);
+        let left = keyboard_input.any_pressed([KeyCode::A]);
+        let right = keyboard_input.any_pressed([KeyCode::D]);
+
+        let y_axis = -(left as i8) + right as i8;
+        let x_axis = -(down as i8) + up as i8;
+
+        let mut move_delta = Vec2::new(y_axis as f32, x_axis as f32);
+
+        match y_axis {
+            1 => {
+                player.state.facing.x = 1.0;
+            }
+            -1 => {
+                player.state.facing.x = -1.0;
+            }
+            _ => {}
         }
-        if keyboard_input.pressed(KeyCode::S) {
-            movement.y -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::A) {
-            player.state.facing.x = -1.0;
-            movement.x -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::D) {
-            player.state.facing.x = 1.0;
-            movement.x += 1.0;
-        }
-        if movement.length() > 0.0 {
+
+        if move_delta != Vec2::ZERO {
             player.state.moving = true;
-            player.state.facing = movement.normalize();
-            transform.translation += movement * player.stats.move_speed * time.delta_seconds();
+            move_delta /= move_delta.length();
         } else {
             player.state.moving = false;
         }
+
+        vel.linvel = move_delta * player.stats.move_speed;
     }
 }
 
 fn move_enemy(
-    time: Res<Time>,
-    mut query: Query<(&mut Enemy, &mut Transform)>,
+    mut enemy_query: Query<(&mut Enemy, &Transform, &mut Velocity)>,
     player_query: Query<&Transform, (With<Player>, Without<Enemy>)>,
 ) {
     let player_transform = player_query.single();
-    for (enemy, mut transform) in &mut query {
+    for (enemy, transform, mut vel) in &mut enemy_query {
         let direction = player_transform.translation - transform.translation;
         let movement = direction.normalize();
-        transform.translation += movement * enemy.stats.move_speed * time.delta_seconds();
+
+        let move_delta = Vec2::new(movement.x, movement.y);
+
+        vel.linvel = move_delta * enemy.stats.move_speed;
+    }
+}
+
+fn hurt_enemy(
+    mut commands: Commands,
+    mut collision_events: EventReader<CollisionEvent>,
+    mut enemy_query: Query<(Entity, &mut Enemy), With<Enemy>>,
+    bullet_query: Query<Entity, With<Bullet>>,
+    rapier_context: Res<RapierContext>,
+) {
+    for collision_event in collision_events.read() {
+        for (enemy_entity, mut enemy) in &mut enemy_query {
+            for bullet_entity in &bullet_query {
+                if let Some(contact_pair) = rapier_context.contact_pair(bullet_entity, enemy_entity)
+                {
+                    // despan bullet
+                    commands.entity(contact_pair.collider1()).despawn();
+
+                    if enemy.stats.health > 0 {
+                        enemy.stats.health -= 1;
+                    } else {
+                        commands.entity(enemy_entity).despawn();
+                    }
+                }
+            }
+        }
+        println!("Received collision event: {:?}", collision_event);
     }
 }
 
@@ -311,21 +370,27 @@ fn player_shoot(
     let mut shooting: bool = false;
     let mut direction: Vec3 = Vec3::ZERO;
     for (player, transform, mut shoot_timer) in &mut query {
+        let offset = 10.0;
+        let mut translation_with_offset = Vec3::ZERO;
         if keyboard_input.pressed(KeyCode::Left) {
             shooting = true;
             direction = Vec3::new(-1.0, 0.0, 0.0);
+            translation_with_offset = transform.translation + Vec3::new(-offset, 0.0, 0.0);
         }
         if keyboard_input.pressed(KeyCode::Right) {
             shooting = true;
             direction = Vec3::new(1.0, 0.0, 0.0);
+            translation_with_offset = transform.translation + Vec3::new(offset, 0.0, 0.0);
         }
         if keyboard_input.pressed(KeyCode::Up) {
             shooting = true;
             direction = Vec3::new(0.0, 1.0, 0.0);
+            translation_with_offset = transform.translation + Vec3::new(0.0, offset, 0.0);
         }
         if keyboard_input.pressed(KeyCode::Down) {
             shooting = true;
             direction = Vec3::new(0.0, -1.0, 0.0);
+            translation_with_offset = transform.translation + Vec3::new(0.0, -offset, 0.0);
         }
         if shooting {
             let duration: f32 = shoot_timer.elapsed_secs();
@@ -348,12 +413,17 @@ fn player_shoot(
                             ..default()
                         },
                         transform: Transform::from_translation(Vec3::new(
-                            transform.translation.x,
-                            transform.translation.y,
+                            translation_with_offset.x,
+                            translation_with_offset.y,
                             0.0,
                         )),
                         ..default()
                     },
+                    RigidBody::Dynamic,
+                    Velocity::zero(),
+                    Collider::ball(bullet.size),
+                    BulletDespawnTimer(Timer::from_seconds(3.0, TimerMode::Once)),
+                    ActiveEvents::COLLISION_EVENTS,
                     bullet,
                 ));
             }
@@ -363,16 +433,21 @@ fn player_shoot(
     }
 }
 
-fn update_bullets(time: Res<Time>, mut query: Query<(&Bullet, &mut Transform)>) {
-    for (bullet, mut transform) in &mut query {
-        transform.translation += bullet.direction * bullet.speed * time.delta_seconds();
-    }
-}
-
-fn enemy_chase_player(
+fn update_bullets(
     mut commands: Commands,
     time: Res<Time>,
-    mut eneym_query: Query<(&Transform, &mut Enemy)>,
-    player_query: Query<&Transform, With<Player>>,
+    mut query: Query<(&Bullet, &mut BulletDespawnTimer, Entity, &mut Velocity)>,
 ) {
+    for (bullet, mut despawn_timer, entity, mut vel) in &mut query {
+        despawn_timer.0.tick(time.delta());
+
+        if despawn_timer.0.just_finished() {
+            commands.entity(entity).despawn();
+        }
+
+        let movement = bullet.direction * bullet.speed;
+        let move_delta: Vec2 = Vec2::new(movement.x, movement.y);
+
+        vel.linvel = move_delta;
+    }
 }
